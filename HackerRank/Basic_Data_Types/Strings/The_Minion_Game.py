fd6e669837c59dc81aceafc6ae05a45cacf5a8f8
7IronSnow7 <divan.debruin25@gmail.com>
2024-11-19 11:28:25 -0500

    Added a soltuion for The Minion Game from Hacker Rank

---
+++ HackerRank/Basic_Data_Types/Strings/The_Minion_Game.py
@@ -0,0 +1,46 @@
+# The Minion Game
+#______________________
+#      INFORMATION
+#______________________
+
+"""_summary_
+Game Rules
+
+Both players are given the same string, .
+Both players have to make substrings using the letters of the string .
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string .
+
+For Example:
+String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+"""
+#______________________
+#      SOLUTION
+#______________________
+
+# Given
+def minion_game(string):
+    # your code goes here
+    vowels = 'AEIOU'
+    stuart_score, kevin_score = 0, 0
+    
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin_score += len(string) - i
+        else:
+            stuart_score += len(string) - i
+            
+    if kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    else:
+        print(f"Stuart {stuart_score}")
+        
+if __name__ == '__main__':
+    s = input("Please input some text: ")
+    minion_game(s)
