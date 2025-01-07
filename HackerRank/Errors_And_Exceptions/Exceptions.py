47a3d4ffc9f91076e4ac4e798428e7ae4986122f
7IronSnow7 <divan.debruin25@gmail.com>
2025-01-07 11:32:19 -0500

    This file has been reverted back to it's original purpose

--- HackerRank/Errors_And_Exceptions/Exceptions.py
+++ HackerRank/Errors_And_Exceptions/Exceptions.py
@@ -50,64 +50,11 @@ Error Code: invalid literal for int() with base 10: '$'
 # link: https://www.hackerrank.com/challenges/exceptions/problem?isFullScreen=true
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-# for _ in range(int(input())):
-#     try:
-#         a, b = input().split()
-#         print(int(a) // int(b))
-#     except ZeroDivisionError as z:
-#         print(f"Error Code: {z}")
-#     except ValueError as v:
-#         print(f"Error Code: {v}")
-
-# Gonna use this file to run some tests for a different issue:
-# def animal_sound_getter(animal_tuples, language):
-#     animal_dict = {}
-    
-#     # each tuple has (language, [(animal, sound)])
-#     # like ("english", [("dog", "woof"), ("cat", "meow")])
-#     for lang, animals in animal_tuples:
-#         if lang == language:
-#             for animal, sound in animals:
-#                 animal_dict[animal] = sound
-
-#     return lambda animal: animal_dict.get(animal, "??")
-
-# # Create our translator
-# animal_data = [
-#     ("english", [("dog", "woof"), ("cat", "meow")]),
-#     ("spanish", [("dog", "guau"), ("cat", "miau")])
-# ]
-
-# english_sounds = animal_sound_getter(animal_data, "english")
-
-# # Use the translator
-# print(english_sounds("dog"))  # prints: "woof"
-# print(english_sounds("cat"))  # prints: "meow"
-# print(english_sounds("fish")) # prints: "??"
-
-# Create a dictionary of superhero powers
-# powers = {
-#     "superman": "flight",
-#     "batman": "money",
-#     "spiderman": "webs",
-#     "boots": "magic"
-# }
-
-# power = lambda hero: powers.get(hero, "no power found")
-# # Try getting some values
-# print(power("superman"))    # This will print flight
-# print(power("wonderwoman")) # No power found
-# print(power("boots"))
-# print(power("flash"))
-
-marvel_rivals = {
-    "dr strange": "vanguard",
-    "human torch": "duelist",
-    "ultron": "strategist"
-}
-
-role = lambda character: marvel_rivals.get(character, "character does not exist")
-
-print(role("dr strange"))
-print(role("hawkeye"))
-print(role("human torch")) 
+for _ in range(int(input())):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except ZeroDivisionError as z:
+        print(f"Error Code: {z}")
+    except ValueError as v:
+        print(f"Error Code: {v}")
